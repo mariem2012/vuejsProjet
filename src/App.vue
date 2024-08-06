d4a7fc85b9256f2d95dba57fb297d0e266aeae78
mariem2012 <mariemdianifaba00@gmail.com>
2024-08-06 10:30:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <BonjourCoach :sub-title="subTitle" />
+  
+
+  <input type="text" v-model="subTitle"> 
+</template>
+
+<script setup>
+  import BonjourCoach from '@/components/BonjourCoach.vue';
+
+  const subTitle =  "Voici ma realisation sur ce projet.";
+</script>
+
+<style scoped>
+
+</style>
+
+
---
+++ src/components/BonjourCoach.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Bonjour Coach</h1>
+  
+  <p>{{ props.subTitle }}</p> 
+ 
+</template>
+
+<script setup>
+  const props = defineProps(['subTitle']);
+
+</script>
+
+
+
